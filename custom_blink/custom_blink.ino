ea7d8cbfc440458cf2851da1bf3db32f65ee5b0a
opzesty <stzu06rel@gmail.com>
2019-11-05 19:40:19 -0500

    Documented pin numbers to NodeMCU board numbers

--- custom_blink/custom_blink.ino
+++ custom_blink/custom_blink.ino
@@ -2,8 +2,7 @@
   Blink
 
   This is a change by Zach
-
-  Turns an LED on for one second, then off for one second, repeatedly.
+Turns an LED on for one second, then off for one second, repeatedly.
 
   Most Arduinos have an on-board LED you can control. On the UNO, MEGA and ZERO
   it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN is set to
@@ -27,13 +26,14 @@
 // the setup function runs once when you press reset or power the board
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
-  pinMode(17, OUTPUT);
+  // D1 on the NodeMCU, see documenation for datasheet 
+  pinMode(5, OUTPUT);
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(17, HIGH);   // turn the LED on (HIGH is the voltage level)
+  digitalWrite(5, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(1000);                       // wait for a second
-  digitalWrite(17, LOW);    // turn the LED off by making the voltage LOW
+  digitalWrite(5, LOW);    // turn the LED off by making the voltage LOW
   delay(1000);                       // wait for a second
 }
